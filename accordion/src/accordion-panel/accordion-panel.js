19ea6c8986a6160ba75c689ae5bc98c001f94566
Tim Wright <tim.wright@10up.com>
2019-03-15 09:16:23 -0400

    added starter key bindings for accordions

--- accordion/src/accordion-panel/accordion-panel.js
+++ accordion/src/accordion-panel/accordion-panel.js
@@ -1,3 +1,5 @@
+import { DOWNARROW, UPARROW, HOME, END } from '@barebones/keycodes';
+
 class accordionPanel extends HTMLElement {
 
 	constructor() {
@@ -60,7 +62,7 @@ class accordionPanel extends HTMLElement {
 		const shadowRoot = self.attachShadow( {
 			mode: 'open'
 		} );
-		
+
 		shadowRoot.innerHTML = templateContent;
 
 		const content = self.shadowRoot.getElementById( 'content' );
@@ -73,17 +75,16 @@ class accordionPanel extends HTMLElement {
 		button.setAttribute( 'aria-controls', 'content' );
 		button.setAttribute( 'aria-expanded', 'false' );
 		button.textContent = self.getAttribute( 'aria-label' );
-		
+
+		// If the aria-label is missing expose a visual error
+		button.textContent ? '' : button.textContent = '(Please set a label for this panel)';
+
 		// If disabled, disabled it
-		if ( self.disabled ) {
-			button.setAttribute( 'disabled', '' );
-		}
-		
+		self.disabled ? button.setAttribute( 'disabled', '' ) : null;
+
 		// If open, open it
-		if ( self.open ) {
-			self.openPanel( button, self.shadowRoot.getElementById( 'content' ) );
-		}
-		
+		self.open ? self.openPanel( button, self.shadowRoot.getElementById( 'content' ) ) : null;
+
 		// Add button to term
 		dt.appendChild( button );
 
@@ -91,37 +92,56 @@ class accordionPanel extends HTMLElement {
 		self.shadowRoot.insertBefore( dt, content );
 
 		// Click event for trigger
-		button.addEventListener('click', () => { self.open = true }, false );
+		button.addEventListener( 'click', () => { self.open = true; }, false );
+
+		// Keyup event for trigger
+		button.addEventListener( 'keyup', ( e ) => {
+
+			switch ( e.keyCode ) {
+					case DOWNARROW:
+						console.log( 'DOWN key was pressed, move to next panel' );
+						break;
+					case UPARROW:
+						console.log( 'UP key was pressed, move to previous panel' );
+						break;
+					case HOME:
+						console.log( 'HOME key was pressed, move to first panel' );
+						break;
+					case END:
+						console.log( 'END key was pressed, move to last panel' );
+						break;
+			}
+		}, false );
 
 	} // constructor()
-	
+
 	togglePanel( self ) {
-		
-		const btn = self.shadowRoot.querySelector('.accordion-action');
-		const controls = self.shadowRoot.querySelector('.accordion-action').getAttribute( 'aria-controls' );
+
+		const btn = self.shadowRoot.querySelector( '.accordion-action' );
+		const controls = self.shadowRoot.querySelector( '.accordion-action' ).getAttribute( 'aria-controls' );
 		const target = self.shadowRoot.getElementById( controls );
 		const state = target.getAttribute( 'aria-hidden' );
-		
-		if ( state === 'true' ) {
+
+		if ( 'true' === state ) {
 
 			self.openPanel( btn, target );
-			target.focus();
-			
+			//target.focus();
+
 		} else {
-			
+
 			self.closePanel( btn, target );
 
 		}
 	} // togglePanel ()
-	
+
 	openPanel( btn, target ) {
 
 		btn.setAttribute( 'aria-expanded', 'true' );
 		target.setAttribute( 'aria-hidden', 'false' );
-		target.setAttribute( 'tabindex', '-1' );
+		target.setAttribute( 'tabindex', '0' );
 
 	} // openPanel()
-	
+
 	closePanel( btn, target ) {
 
 		btn.setAttribute( 'aria-expanded', 'false' );
@@ -129,49 +149,37 @@ class accordionPanel extends HTMLElement {
 		target.removeAttribute( 'tabindex' );
 
 	} // closePanel()
-	
+
 	// Getter for the open property
 	get open() {
-
 		return this.hasAttribute( 'open' );
-
 	}
-	
+
 	set open( val ) {
 
 		// Reflect the value of the open property as an HTML attribute.
 		if ( val ) {
-
 			this.setAttribute( 'open', '' );
-
 		} else {
-
 			this.removeAttribute( 'open' );
-
 		}
-		
+
 		this.togglePanel( this );
 
 	}
 
 	// Getter for the disabled property
 	get disabled() {
-
-		return this.hasAttribute('disabled');
-
+		return this.hasAttribute( 'disabled' );
 	}
-	
+
 	set disabled( val ) {
 
 		// Reflect the value of the disabled property as an HTML attribute.
 		if ( val ) {
-
 			this.setAttribute( 'disabled', '' );
-
 		} else {
-
 			this.removeAttribute( 'disabled' );
-
 		}
 
 	}
